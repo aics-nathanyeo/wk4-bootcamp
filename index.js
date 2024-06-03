d146e999cb807a8dc0f88e27d79670b7ea91c0f4
Nathan Yeo <nathan_yeo@asus.com>
2024-06-03 10:25:44 +0800

    feat: added redis cache

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 import express from 'express';
 import pg from 'pg';
+import redis from 'redis';
 
 const { Pool } = pg;
 const app = express();
 app.use(express.json());
 const pool = new Pool();
+const cacheHostName = process.env.AZURE_CACHE_FOR_REDIS_HOST_NAME;
+const cachePassword = process.env.AZURE_CACHE_FOR_REDIS_ACCESS_KEY;
+if (!cacheHostName || !cachePassword) {
+  console.error('Missing redis environment variables');
+  process.exit(1);
+}
+const cacheConnection = redis.createClient({
+  // rediss for TLS
+  url: `rediss://${cacheHostName}:6380`,
+  password: cachePassword,
+});
+await cacheConnection.connect();
+console.log('Connected to Redis');
+await cacheConnection.flushAll();
+console.log('Cache flushed');
 
 app.post('/calculate', async (req, res) => {
   try {
@@ -15,15 +31,27 @@ app.post('/calculate', async (req, res) => {
     if (isNaN(num1Parsed) || isNaN(num2Parsed)) {
       return res.status(400).send('Invalid input');
     }
-
-    const result = num1Parsed + num2Parsed;
+    const cacheKey = `${num1Parsed}:${num2Parsed}`;
+    const cachedResult = await cacheConnection.get(cacheKey);
+    // we assume that this process is the limiting factor
+    const getResult = async () => {
+      if (cachedResult) {
+        console.log('cache hit');
+        return Number(cachedResult);
+      }
+      console.log('cache miss');
+      const calcRes = num1Parsed + num2Parsed;
+      await cacheConnection.set(cacheKey, calcRes);
+      return calcRes;
+    };
+    const result = await getResult();
 
     const client = await pool.connect();
     try {
-      await client.query('INSERT INTO hist_log(num1, num2) VALUES($1, $2)', [
-        num1Parsed,
-        num2Parsed,
-      ]);
+      await client.query(
+        'INSERT INTO hist_log(num1, num2, result) VALUES($1, $2, $3)',
+        [num1Parsed, num2Parsed, result]
+      );
     } catch (err) {
       console.error('Error inserting log', err);
       throw err;
@@ -37,7 +65,7 @@ app.post('/calculate', async (req, res) => {
   }
 });
 
-app.get('/hist_log', async (req, res) => {
+app.get('/hist_log', async (_, res) => {
   try {
     const client = await pool.connect();
     const dbResult = await client.query(
@@ -47,7 +75,7 @@ app.get('/hist_log', async (req, res) => {
       dbResult.rows.map((row) => ({
         num1: Number(row.num1),
         num2: Number(row.num2),
-        result: Number(row.num1) + Number(row.num2),
+        result: Number(row.result),
       }))
     );
     client.release();
@@ -59,6 +87,7 @@ app.get('/hist_log', async (req, res) => {
 
 app.get('/', async (req, res) => {
   res.send('Hello World!\n');
+  console.log('cache response' + (await cacheConnection.ping()));
 });
 
 app.listen(process.env.PORT, () => {
@@ -66,7 +95,6 @@ app.listen(process.env.PORT, () => {
 });
 
 process.on('exit', async () => {
-  console.log('test');
+  cacheConnection.disconnect();
   await pool.end();
-  console.log('test2');
 });
